07dffb096e400ae31998a44004ff7e863ce0730f
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-18 17:54:18 -0300

    Work in progress

--- src/com/expenseTracker/app/Main.java
+++ src/com/expenseTracker/app/Main.java
@@ -1,23 +1,36 @@
 package com.expenseTracker.app;
 
 import java.util.ArrayList;
+//import com.expenseTracker.*;
 
 class Main {
-
-}
-class Board {
-    private Expense expense;
-
-    public Board(ArrayList<Expense> expenseArrL) {
+    public static void main(String[] args) {
+        Print.printLn("Expense Tracker v0.1 by Henrique\n");
 
     }
+}
+class Board {
+    private ArrayList<Expense> expenses;
 
+    public Board(ArrayList<Expense> expenseArrL, String name) {
+
+    }
+    public void addExpense(Expense expense, String name, int cost) {
+        this.expenses = expenses.add();
+    }
 }
 class Expense {
-    private String title;
+    private String name;
+    private int cost;
 
-    
-    public String getTitle() {
-        return this.title;
+    public Expense(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public Expense addExpense(String name, int cost) {
+        return new Expense(name, cost);
     }
 }
---
+++ src/com/expenseTracker/app/Print.java
@@ -0,0 +1,10 @@
+package com.expenseTracker.app;
+
+public class Print {
+    public static void printLn(Object text) {
+        System.out.println(text);
+    }
+    public static void print(Object text) {
+        System.out.println(text);
+    }
+}
