9db7a2db80836324af7a0ac215402579d1891a21
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-18 16:33:55 -0300

    Work in progress

--- src/com/expenseTracker/app/Main.java
+++ src/com/expenseTracker/app/Main.java
@@ -1,11 +1,20 @@
 package com.expenseTracker.app;
 
+import java.util.ArrayList;
+
 class Main {
 
 }
-class Workspace {
-    
+class Board {
+    public Board() {
+
+    }
 }
 class Expense {
-
+    private ArrayList<String> title = new ArrayList<>();
+    private short i = 0;
+    public Expense(String title) {
+        this.title.add(title);
+        i++;
+    }
 }
