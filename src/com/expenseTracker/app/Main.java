1bf726135ee329ce0b82cfaa0d128bf1e1b5a6f8
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-20 19:57:35 -0300

    Work in progress

--- src/com/expenseTracker/app/Main.java
+++ src/com/expenseTracker/app/Main.java
@@ -10,15 +10,15 @@ class Main {
 
     public static void main(String[] args) {
         boolean entryPointFlag = true, secondFlag = true;
-        short choice, currentBoard = -1;
+        short choice, currentBoard = -1, boardCount = 0;
         int monthlyWage, expenseCost;
-        String boardName, expenseTitle;
+        String boardName, expenseTitle, setActiveStr;
 
         Print.printLn("Expense Tracker v0.1 by Henrique\n");
         while (entryPointFlag) { //MAIN LOOP
             Print.print("=".repeat(10) + "MAIN MENU" + "=".repeat(10) + "\n" +
-                    "1 - Create new board\n2 - Insert new expense\n" + "3 - Delete expense\n" +
-                            "4 - List expenses\n0 - Exit\n" + "=".repeat(29) + "\n---> ");
+                    "1 - Create new board\n2 - Set active board\n3 - Insert new expense\n" +
+                    "4 - Delete expense\n5 - List expenses\n0 - Exit\n" + "=".repeat(29) + "\n---> ");
 
             choice = Short.parseShort(scan.nextLine());
             if (choice == 0) {
@@ -32,27 +32,62 @@ class Main {
                 Print.print("Enter the monthly wage: ");
                 monthlyWage = Short.parseShort(scan.nextLine());
                 boardArrL.add(new Board(boardName, monthlyWage));
-                currentBoard++;
-                Print.printLn("You just created board No. " + currentBoard +
-                        " with the name '" + boardArrL.get(currentBoard).getName() + "'.");
-                continue;
+                boardCount++;
+                Print.printLn("You just created board No. " + boardCount +
+                        " with the name '" + boardArrL.get(boardCount - 1).getName() + "'.");
+                Print.print("Set newly created board as current active board? ('Y' or 'N')\n--> ");
+                setActiveStr = scan.nextLine();
+                if (setActiveStr.equalsIgnoreCase("y")) {
+                    currentBoard = (short) (boardCount - 1);
+                    Print.printLn("*".repeat(29));
+                    continue;
+                }
+                else if (setActiveStr.equalsIgnoreCase("n"))
+                    continue;
             }
             else if (choice == 2) {
-                Print.print("Expense title: ");
-                expenseTitle = scan.nextLine();
-                Print.print("Cost: ");
-                expenseCost = Short.parseShort(scan.nextLine());
-                boardArrL.get(currentBoard).insertExpense(expenseTitle, expenseCost);
-                Print.printLn("'" + expenseTitle + "' added to board No. " + currentBoard + ".");
-                continue;
+                Print.printLn("_".repeat(29) + "\nSET ACTIVE BOARD");
+                Print.print("Current boards: ");
+                for (short i = 0; i < boardArrL.size(); i++) {
+                    Print.print((i + 1) + " | ");
+                }
+                Print.print("\nWhich board would you like to be focused?\n--> ");
+                currentBoard = Short.parseShort(scan.nextLine());
+                currentBoard--;
+                Print.printLn("Focusing board No. " + (currentBoard + 1) + ".\n" + "*".repeat(29));
+            }
+            else if (choice == 3) {
+                while(true) {
+                    if (currentBoard != -1) {
+                        Print.printLn("_".repeat(29) + "\nEXPENSE INSERTION");
+
+                        Print.print("Board No. " + (currentBoard + 1) + " selected.\nExpense title: ");
+                        expenseTitle = scan.nextLine();
+                        Print.print("Cost: ");
+                        expenseCost = Short.parseShort(scan.nextLine());
+                        boardArrL.get(currentBoard).insertExpense(expenseTitle, expenseCost);
+                        Print.printLn("'" + expenseTitle + "' added to board No. " + (currentBoard + 1) + ".");
+                        Print.printLn("*".repeat(29));
+                        break;
+                    }
+                    else {
+                        Print.print("_".repeat(29) + "\nTo which board will this expense belong to?\n--> ");
+                        currentBoard = Short.parseShort(scan.nextLine());
+                        currentBoard--;
+                    }
+                }
+            }
+            else if (choice == 4) {
+                Print.print("");
             }
             else {
                 Print.printLn("Invalid option. Try again.");
+                Print.printLn("*".repeat(29));
                 secondFlag = false;
             }
-            while (secondFlag) {
-                
-            }
+            /*while (secondFlag) {
+
+            }*/
         }
     }
 
